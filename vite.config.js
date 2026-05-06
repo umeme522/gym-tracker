25707169b53273eb977121070a8641aeab650f33
umeme522 <yutaro.umehara@gmail.com>
2026-05-06 12:22:29 +0900

    Fix black screen issue by correcting base URL to /gym-tracker/

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: '/AppPortal/%E3%82%B8%E3%83%A0%E8%A8%98%E9%8C%B2%E3%82%A2%E3%83%97%E3%83%AA/',
+    base: '/gym-tracker/',
 })
